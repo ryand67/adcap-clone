94b0c69851fe4df52d31164ac5be7842a16a3b29
ryand67 <rwdevenney@gmail.com>
2021-01-04 18:38:54 -0500

    comments

--- components/Worker/worker.js
+++ components/Worker/worker.js
@@ -2,23 +2,42 @@ import style from './worker.module.css';
 import getProductivity from './workerUtil/getProductivity';
 import { useState } from 'react';
 
-const Worker = ({ updateScore, currentScore, productivityLevel, name, unlocked }) => {
-    const [productivity, setProductivity] = useState(getProductivity(name));
+const Worker = ({ updateScore, currentScore, productivityLevel, name, unlocked, automatedProp }) => {
 
+    //States
+    //Productivity/score gained per action
+    const [productivity, setProductivity] = useState(getProductivity(name));
+    //Status of whether or not the worker is working or idle
+    const [workStatus, setWorkStatus] = useState(false);
+    //Flag for whether the worker is automated.
+    const [automated, setAutomated] = useState(automatedProp || false);
+
+    //Works and updates score
     const work = () => {
         updateScore(currentScore + productivity);
     }
 
+    //Unlocks/buys a worker
     const buyWorker = () => {
 
     }
 
+    //Automates a worker
+    const automate = () => {
+        setAutomated(true);
+    }
+
     return (
         <div className={style.worker}>
+            {/* Name */}
             <h2>{name}</h2>
-            {unlocked ? <button onClick={work}>Click to Work</button> : <button onClick={buyWorker}>Purchase Worker</button>}
+            {/* Checks status and if they're working displays that the worker is working or idle */}
+            <h2 className={style.status}>{workStatus ? 'Working' : 'Idle'}</h2>
+            {/* Checks if the worker is unlocked, and whether it's automated and displays the appropriate buttons */}
+            {unlocked ? (automated ? <h2>Automated</h2> : <button onClick={work}>Click to Work</button>) : <button onClick={buyWorker}>Purchase Worker</button>}
+            {/* Upgrade button */}
             <button>Upgrade</button>
-            <button>Automate</button>
+            {automated ? <></> : <button onClick={automate}>Automate</button>}
         </div>
     )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,17 @@
-import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 import Worker from '../components/Worker/worker';
 import { useState } from 'react';
 
 export default function Home() {
+  //Total score
   const [testScore, setScore] = useState(0);
 
   return (
     <div className={styles.container}>
-      <h1>${testScore}</h1>
-      <Worker currentScore={testScore} updateScore={setScore} productivityLevel={1} unlocked={true} name="Basic Worker" price={0} />
+      {/* display score */}
+      <h1>${testScore.toFixed(2)}</h1>
+      {/* Worker components */}
+      <Worker currentScore={testScore} updateScore={setScore} productivityLevel={1} unlocked={true} name="Basic Worker" price={0} automatedProp={false} />
     </div>
   )
 }
