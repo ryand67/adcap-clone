99aa4dd963049a410e9e238a55020cda267b3a38
ryand67 <rwdevenney@gmail.com>
2021-01-04 22:43:55 -0500

    title score

--- pages/index.js
+++ pages/index.js
@@ -5,19 +5,19 @@ import Head from 'next/head';
 
 export default function Home() {
   //Total score
-  const [testScore, setScore] = useState(0);
+  const [currentScore, setScore] = useState(0);
 
   return (
     
     <div className={styles.container}>
       <Head>
-        <title>AdCap Clone</title>
+        <title>AdCap Clone: ${currentScore}</title>
         <link rel="shortcut icon" href="https://cdn0.iconfinder.com/data/icons/social-messaging-productivity-4/128/dollar-sign-512.png" type="image/x-icon"/>
       </Head>
       {/* display score */}
-      <h1>${testScore.toFixed(2)}</h1>
+      <h1>${currentScore.toFixed(2)}</h1>
       {/* Worker components */}
-      <Worker currentScore={testScore} updateScore={setScore} unlocked={true} name="Basic Worker" price={0} automatedProp={false} automatePrice={1} />
+      <Worker currentScore={currentScore} updateScore={setScore} unlocked={true} name="Basic Worker" price={0} automatedProp={false} automatePrice={1} />
     </div>
   )
 }
